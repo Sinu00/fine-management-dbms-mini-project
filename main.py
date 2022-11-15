68a0b0154160d4e15abe8f1f38a68fa3e710957d
rabeeh-ta <rabeehchemnad@gmail.com>
2022-11-15 19:36:19 +0530

    grid() to place() ui positioning

--- main.py
+++ main.py
@@ -16,37 +16,46 @@ root.geometry('750x600')
 
 #? different pages of the app
 notebook = Notebook()
-homePage = Frame(root, border=180)
+homePage = Frame(root)
 profilePage = Frame(root)
 notebook.add(homePage, text="Fine")
 notebook.add(profilePage, text="Search")
-notebook.grid(row=0, column=1)
+notebook.place(x=10, y=5, width=750, height=600)
 
 #? Fine page widgets
 # usn input
-usnLabel = Label(homePage, text="Student USN", width=15).grid(row=1, column=0)
+usnLabel = Label(homePage, text="Student USN", width=15).place(x=150, y=15)
 usnVar = StringVar()
-usnInput = Entry(homePage, textvariable=usnVar).grid(row=1, column=1)
+usnInput = Entry(homePage, textvariable=usnVar).place(x=260,
+                                                      y=10,
+                                                      width=200,
+                                                      height=30)
 
 # fined by => faculty info
-facultyIdLabel = Label(homePage, text="Faculty Id", width=15).grid(row=2,
-                                                                   column=0)
+facultyIdLabel = Label(homePage, text="Faculty Id", width=15).place(x=150,
+                                                                    y=55)
 facultyIdVar = StringVar()
-facultyIdInput = Entry(homePage, textvariable=usnVar).grid(row=2, column=1)
+facultyIdInput = Entry(homePage, textvariable=usnVar).place(x=260,
+                                                            y=50,
+                                                            width=200,
+                                                            height=30)
 # fine amount input
-fineAmountLabel = Label(homePage, text="Fine Id", width=15).grid(row=3,
-                                                                 column=0)
+fineAmountLabel = Label(homePage, text="Fine Id", width=15).place(x=150, y=95)
 fineAmountVar = StringVar()
-fineAmountInput = Entry(homePage, textvariable=fineAmountVar).grid(row=3,
-                                                                   column=1)
+fineAmountInput = Entry(homePage, textvariable=fineAmountVar).place(x=260,
+                                                                    y=90,
+                                                                    width=200,
+                                                                    height=30)
 # fine reason input
 fineReasonLabel = Label(homePage, text="Fine Description",
-                        width=15).grid(row=4, column=0)
+                        width=15).place(x=150, y=135)
 fineReasonVar = StringVar()
-fineReasonInput = Entry(homePage, textvariable=fineReasonVar).grid(row=4,
-                                                                   column=1)
+fineReasonInput = Entry(homePage, textvariable=fineReasonVar).place(x=260,
+                                                                    y=130,
+                                                                    width=200,
+                                                                    height=60)
 submitButton = Button(homePage, text="Fine Confirm",
-                      command=submit).grid(row=5, column=1)
+                      command=submit).place(x=280, y=200)
 
 #? Search Student Page Widgets
 
