35ca8274cdf09291b954dc0b9a6f37bc69b49bb5
rabeeh-ta <rabeehchemnad@gmail.com>
2022-11-21 21:45:59 +0530

    new schema datatypes from sqlite docs

--- querys.sql
+++ querys.sql
@@ -1,41 +1,55 @@
+PRAGMA foreign_keys = ON; -- foreign_keys are not turned on in sqlite by default
+
 -- student table
 CREATE TABLE students(
-    usn  VARCHAR(10) PRIMARY KEY NOT NULL,
-    name VARCHAR(100),
-    phone CHAR(10), 
-    branch CHAR(2)
+    usn  TEXT PRIMARY KEY NOT NULL,
+    name TEXT,
+    phone INTEGER, 
+    branch TEXT
 );
 
 -- faculties
 CREATE TABLE faculties(
-    id INT PRIMARY KEY NOT NULL,
-    name VARCHAR(100)
-) 
+    id INTEGER PRIMARY KEY NOT NULL,
+    name TEXT
+); 
 
 -- department level fines
 CREATE TABLE department_fines(
-    id INT PRIMARY KEY NOT NULL,
-    description VARCHAR(100),
-    amount INT
+    id INTEGER PRIMARY KEY NOT NULL,
+    description TEXT,
+    amount INTEGER
 );
 
 -- college level fines
 CREATE TABLE college_fines(
-    id INT PRIMARY KEY NOT NULL,
-    description VARCHAR(100),
-    amount INT
+    id INTEGER PRIMARY KEY NOT NULL,
+    description TEXT,
+    amount INTEGER
 );
 
 -- fines table
 CREATE TABLE fines(
-    id CHAR(21) PRIMARY KEY,
-    usn CHAR(10),
-    date CHAR(8),
-    amount INT,
+    id TEXT PRIMARY KEY,
+    usn TEXT,
+    date TEXT,
     paid BOOLEAN NOT NULL,
-    fine_id INT, -- foreign key -> about the fine
+    fine_id INTEGER, 
     description TEXT,
-    fined_by INT, -- foreign key -> of falculty fined
-    FOREIGN KEY (fine_id) REFERENCES department_fines(id) REFERENCES college_fines(id),
+    fined_by INTEGER, 
+    FOREIGN KEY (usn) REFERENCES students(usn),
+    FOREIGN KEY (fine_id) REFERENCES department_fines(id)
+    , FOREIGN KEY (fine_id) REFERENCES college_fines(id),
     FOREIGN KEY (fined_by) REFERENCES faculties(id)
 );
+
+-----
+CREATE TABLE students(usn TEXT PRIMARY KEY, name TEXT);
+CREATE TABLE fines(
+     usn TEXT , 
+     description TEXT, 
+    FOREIGN KEY(usn) REFERENCES students(usn)
+);
+
+insert into students values("4PA20CS094", "RABEEH T A");
+insert into fines values("4PA20CS094", "this is some big description about the thing");
